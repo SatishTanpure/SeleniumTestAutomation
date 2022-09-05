dc02cf6fd23f240f394bf65a70c1349e67800069
SatishTanpure <satish.tanpure@yahoo.com>
2022-09-05 23:29:41 +0530

    first commit

---
+++ TestAutomationProject/LoginPage.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAutomationProject
+{
+    internal class LoginPage
+    {
+
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement username => driver.FindElement(By.Id("email")); 
+        private IWebElement password => driver.FindElement(By.Id("pass"));
+        private IWebElement loginButton => driver.FindElement(By.Name("login"));
+        private IWebElement errorMSG => driver.FindElement(By.XPath("//div[contains(text(),'Wrong credentials')]"));
+
+        public void EnterLoginDetails(String _username, string _password)
+        {
+            username.SendKeys(_username);
+            password.SendKeys(_password);
+            loginButton.Click();
+        }
+
+        public void ValidateData(String ExpectedErrorMessage)
+        {
+            string ExpectedMessage = ExpectedErrorMessage;
+            string ActualMesssage = errorMSG.Text;
+
+            if (ExpectedMessage.Equals(ActualMesssage))
+            {
+                Assert.Pass();
+            }else
+            {
+                Assert.Fail();
+            }
+
+        }
+
+    }
+}
---
+++ TestAutomationProject/UnitTest1.cs
@@ -0,0 +1,39 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestAutomationProject
+{
+    public class Tests
+    {
+
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(@"C:\Users\Admin\Downloads\chromedriver_win32");
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+        }
+
+        [Test]
+        public void Test1()
+        {
+            LoginPage LP = new LoginPage(driver);
+
+            LP.EnterLoginDetails("dfsf", "sfs");
+
+            Thread.Sleep(5000);
+
+            LP.ValidateData("Wrong credentials");
+            
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+    }
+}
